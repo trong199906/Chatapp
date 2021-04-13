eb0d15aa7a6ea931d7c01e045b2b036f962b7ef4
TrongND <interns@arius.com.vn>
2021-04-13 16:27:32 +0700

    this add for a chat

---
+++ justchat/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for justchat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justchat.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+  # Just HTTP for now. (We can add other protocols later.)
+})
